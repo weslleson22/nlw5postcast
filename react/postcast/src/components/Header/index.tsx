abe369c25db33973fa40cb42eaea7b505321f8d5
wesleson22 <wesllesonsowza@gmail.com>
2021-04-27 12:47:41 -0300

    Componente Header

--- react/postcast/src/components/Header/index.tsx
+++ react/postcast/src/components/Header/index.tsx
@@ -1,7 +1,15 @@
-export function Header(){
+ import format from 'date-fns/format';
+ import ptBR from 'date-fns/locale/pt-BR';
+ import styles from './styles.module.scss';
+ export function Header(){
+    //para pegar a data atual... foi add yarn add date-fns
+    const currenteDate = format(new Date(), 'EEEEEE, d MMMM',{locale: ptBR} );
+    //termina aqui 
     return(
-        <header>
+        <header className={styles.headerContainer}>
         <img src="/logo.svg" alt="Podcastr"/>
+        <p>O melhor para você Ouvir</p>
+        <span>{currenteDate}</span>
         </header>
 
     );
