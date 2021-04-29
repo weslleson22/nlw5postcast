d79b9e6ce9446d1771e3536cf15151b976069001
wesleson22 <wesllesonsowza@gmail.com>
2021-04-29 11:16:51 -0300

    Comsumindo API

--- react/postcast/src/pages/index.tsx
+++ react/postcast/src/pages/index.tsx
@@ -1,6 +1,23 @@
- 
-export default function Home() {
-  return (
-    <h1>Index</h1>
+//spa
+//ssr
+//ssg
+export default function Home(props){
+  return(
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+
+    </div>
   )
+}
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return{
+    props:{
+      episodes:data,
+    },
+    revalidate:60*60*8,
+  }
+
 }
