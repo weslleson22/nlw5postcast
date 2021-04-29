0867f42772e2082e4d9f871e9e467e9f1a2f1190
wesleson22 <wesllesonsowza@gmail.com>
2021-04-28 22:21:18 -0300

    styles menu do play

--- react/postcast/src/components/Player/index.tsx
+++ react/postcast/src/components/Player/index.tsx
@@ -10,10 +10,12 @@
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
 
-            <footer>
+            <footer className={styles.empty}>
                 <div className={styles.progress}>
                 <span>00:00</span>
-                <div className={styles.emptySlider}/>
+                <div className={styles.slider}>
+                    <div className={styles.emptySlider}/>
+                </div>
                 <span>00:00</span>
                 </div>
                 <div className={styles.buttons}>
